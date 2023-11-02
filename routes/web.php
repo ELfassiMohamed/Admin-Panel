55963cf7a0187075ae0e4be417d1404bff6fa615
ELfassiMohamed <elfassimohamed2001@gmail.com>
2023-11-02 16:46:43 +0100

    11/2/2023

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,9 @@ Route::get('/', function () {
     ]);
 });
 
+
+
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
