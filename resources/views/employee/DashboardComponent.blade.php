634607acad35111c4d2d26cf9e0d6970590ae622
ELfassiMohamed <elfassimohamed2001@gmail.com>
2024-01-23 14:39:29 +0100

    commit

--- resources/views/employee/DashboardComponent.blade.php
+++ resources/views/employee/DashboardComponent.blade.php
@@ -8,6 +8,7 @@
     <title>Dashboard</title>
 </head>
 <body>
+  @include('employee.navbar')
     <div class="p-10 m-10" >
         <div class="px-4 sm:px-0">
           <h3 class="text-base font-semibold leading-7 text-gray-900">EmployeeInformation</h3>
